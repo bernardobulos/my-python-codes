6cd5e9963284750c914bbb6ec7dbac538ae65837
Bernardo <81765951+bernardobulos@users.noreply.github.com>
2025-11-25 16:56:32 -0500

    Add Tkinter GUI with entry and button functionalities

---
+++ python_tkinter/aula04.py
@@ -0,0 +1,46 @@
+from tkinter import *
+
+# entry widget (entrada widget) = uma caixa de texto que aceita uma única linha de entrada do usuário.
+
+def submit():
+    username = entry.get()
+    print(f"Hello, {username}!")
+    # entry.config(state=DISABLED)
+
+def delete():
+    entry.delete(0, END)
+
+def backspace():
+    entry.delete(len(entry.get()) - 1, END)
+
+window = Tk()
+
+entry = Entry(
+    window,
+    font=("Arial", 50),
+    fg="#00FF00",
+    bg="black",
+    # show="*"
+    )
+# entry.insert(0, "Spider-Man")
+entry.pack(side=LEFT)
+
+submit_button = Button(
+    window,
+    text="submit",
+    command=submit)
+submit_button.pack(side=RIGHT)
+
+delete_button = Button(
+    window,
+    text="delete",
+    command=delete)
+delete_button.pack(side=RIGHT)
+
+backspace_button = Button(
+    window,
+    text="backspace",
+    command=backspace)
+backspace_button.pack(side=RIGHT)
+
+window.mainloop()
