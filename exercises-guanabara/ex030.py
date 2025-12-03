06e70fec5dcb3111f43e663bde2aa383ff30c4e2
Bernardo <81765951+bernardobulos@users.noreply.github.com>
2025-12-03 11:06:57 -0500

    Add even or odd checker program in ex030.py
    Implemented a program to check if an integer is even or odd.

---
+++ exercises-guanabara/ex030.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR. 🇧🇷
+# Write a program that reads an integer and displays on the screen whether it is EVEN or ODD. 🇺🇸
+# Escriba un programa que lea un número entero y muestre en la pantalla si es PAR o IMPAR. 🇪🇸
+# Écrivez un programme qui lit un entier et affiche à l'écran s'il est PAIR ou IMPAIR. 🇫🇷
+
+num = int(input("Enter a integer: "))
+
+if num % 2 == 0:
+    print(f"The integer {num} is even.")
+else:
+    print(f"The integer {num} is odd.")
