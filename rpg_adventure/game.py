31194ac55907dd5d42de82e8a574ccd57053268a
Bernardo <81765951+bernardobulos@users.noreply.github.com>
2026-01-08 13:19:22 -0500

    Enhance user experience with delays in game flow
    Added sleep function for better user experience during character selection and battle.

--- rpg_adventure/game.py
+++ rpg_adventure/game.py
@@ -1,26 +1,33 @@
 from personagens import *
+from time import sleep
 
 hero = Guerreiro("Hero", 120, 18, 6)
 merlin = Mago("Merlin", 70, 25, 2)
 slime = PersonagemBasico("Slime", 40, 7, 3)
 
 while True: # Escolha de Personagem
+    print("\nPara selecionar um personagem, digite somente seu número correspondente.")
     print("\nPERSONAGENS DISPONÍVEIS:\n[1] Hero\n[2] Merlin")
     escolha = input("Escolha seu personagem: ").strip()[0]
     if escolha == "1":
         jogador = hero
-        print(f"Você escolheu {jogador.nome}!\n")
+        print(f"Você escolheu \033[33m{jogador.nome}\033[m!\n")
+        sleep(1)
         break
     elif escolha == "2":
         jogador = merlin
-        print(f"Você escolheu {jogador.nome}!\n")
+        print(f"Você escolheu \033[33m{jogador.nome}\033[m!\n")
+        sleep(1)
         break
     else:
-        print("Opção inválida, tente novamente.")
+        print("\033[31mOpção inválida, tente novamente.\033[m")
+        sleep(1)
         continue
 
 jogador.mostrar_status()
+sleep(1)
 slime.mostrar_status()
+sleep(1)
 
 rodada = 1
 turno_jogador = 1
